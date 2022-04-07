e06590a7eb15f68730b1c59ec3cc7a4b8f278860
abhimnyu <abhimanyukumar671@gmal.com>
2022-04-07 14:02:23 +0530

    init commit

---
+++ tictactoe/src/App.jsx
@@ -0,0 +1,29 @@
+// import React from "react";
+
+// export default () => (
+//   <>
+//     <h1>Welcome to React Vite Micro App!</h1>
+//     <p>Hard to get more minimal than this React app.</p>
+//   </>
+// );
+
+// import React from 'react'
+
+// export default function App() {
+//   return (
+//     <div>App</div>
+//   )
+// }
+
+import React from 'react'
+
+function App() {
+  return (
+    <>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+       </>
+  )
+}
+
+export default App
